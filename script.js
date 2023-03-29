b7e4dd3602ebfa575b2788be80c56025b957283e
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-03-29 18:37:27 +0700

    Add a button that changes size of canvas

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function changeColor(e) {
-   e.target.classList.add("hovered");
+    e.target.classList.add("hovered");
 
 }
 
@@ -7,28 +7,41 @@ function removeColor(e) {
     e.target.classList.remove("hovered");
 }
 
+function makeGrid(size) {
+    const div = document.createElement("div");
+    div.classList.add("column");
+    //Create a 16x16 div square
+
+    for (i = 0; i < size; i++) {
+        div.appendChild(document.createElement("div"));
+    }
 
 
+    for (j = 0; j < size; j++) {
+        container.appendChild(div.cloneNode(true));
+    }
+
+    const boxes = document.querySelectorAll(".column > div");
+
+    boxes.forEach((box) => {
+        // Add eventlistens for both hover state and when trasition ends.
+        box.addEventListener('mouseover', changeColor);
+        box.addEventListener('transitionend', removeColor);
+    })
+}
 const container = document.querySelector(".container");
-const div = document.createElement("div");
+const button = document.querySelector("button");
 
-div.classList.add("column");
+makeGrid(16);
 
-console.log(container);
+button.addEventListener("click", (e) => {
+    let newSize = 16;
 
-for (i = 0; i < 16; i++){
-    div.appendChild(document.createElement("div"));
-}
+    do  { 
+    newSize = prompt("Specify the number of squares in each row", 16);
+    } while(!(newSize <= 100 && newSize > 0))
 
+    container.innerHTML = ""
+    makeGrid(newSize);
 
-for (j = 0; j < 16; j++){
-    container.appendChild(div.cloneNode(true));
-}
-
-const boxes = document.querySelectorAll(".column > div");
-
-boxes.forEach((box) => {
-    box.addEventListener('mouseover', changeColor);
-    box.addEventListener('transitionend', removeColor);
-})
-
+} )
